3088ed20c6490328abda3f5c04f4ef7c000baa17
reshma <reshmapatan915@gmail.com>
2025-05-14 22:06:54 +0530

    Add files via upload

--- logout.php
+++ logout.php
@@ -1,7 +1,11 @@
 <?php
 session_start(); // Start the session
+
+// Destroy the session
 session_unset(); // Unset all session variables
 session_destroy(); // Destroy the session
-header("Location: login.php"); // Redirect to the admin login page
-exit(); // Ensure no further code executes
+
+// Redirect to the login page
+header('Location: login.php');
+exit();
 ?>
